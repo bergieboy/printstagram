c8b8289a0512de1c685aa8cff919b7b94067c43d
Todd <toddleebergman@gmail.com>
2018-04-03 21:05:07 -0700

    Completed Root file and refactored the store accordingly

--- instagram/frontend/entry.jsx
+++ instagram/frontend/entry.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { login, logout, signup } from './util/session_api_util';
+import configureStore from './store/store';
+import Root from './components/root';
+// import { login, logout, signup } from './util/session_api_util';
 
-document.addEventListener('DOMContentLoaded', () =>{
+document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+
+  // Testing
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  // Testing
+  
   const root = document.getElementById('root');
-  window.login = login;
-  window.logout = logout;
-  window.signup = signup;
-  ReactDOM.render(<h1>Printstagram</h1>, root);
+  ReactDOM.render(<Root store={store}/>, root);
 });
