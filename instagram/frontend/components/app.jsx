65fa72974bf08efe12e34974f6aa5655ddd3a029
Todd <toddleebergman@gmail.com>
2018-04-09 19:29:21 -0700

    replaced the app.jsx file to fix heroku issue

---
+++ instagram/frontend/components/app.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import {
+  Route,
+  Redirect,
+  Switch,
+  Link,
+  HashRouter
+} from 'react-router-dom';
+import { AuthRoute, ProtectedRoute } from './../util/route_util';
+
+import NavBarTempContainer from './navbar/navbar_container';
+import LoginFormContainer from './session_form/login_form_container';
+import SignupFormContainer from './session_form/signup_form_container';
+import UserProfileContainer from './user_profile/user_profile_container';
+import EditProfileContainer from './user_profile/edit_profile_container';
+import PhotoUploadContainer from './photos/photo_upload_container';
+import PhotoFeedContainer from './photos/photo_feed_container';
+import PhotoShowContainer from './photos/photo_show_container';
+import Modal from './modal';
+
+const App = () => (
+
+  <div>
+    <Modal />
+    <header>
+      <ProtectedRoute path="/" component={NavBarTempContainer} />
+    </header>
+    <Switch>
+      <AuthRoute exact path="/login" component={LoginFormContainer} />
+      <AuthRoute exact path="/signup" component={SignupFormContainer} />
+      <Route exact path="/" component={PhotoFeedContainer} />
+      <Route exact path="/new" component={PhotoUploadContainer} />
+      <Route exact path="/:userId" component={UserProfileContainer} />
+      <Route exact path="/photos/:photoId" component={PhotoShowContainer}/>
+      <Route exact path="/:userId/edit" component={EditProfileContainer} />
+    </Switch>
+  </div>
+);
+
+export default App;
