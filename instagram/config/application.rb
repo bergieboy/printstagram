2c93cb109652fb917423caf0d620996f1c2d8483
Todd <toddleebergman@gmail.com>
2018-04-11 13:03:35 -0700

    changed s3 region in application.rb file

--- instagram/config/application.rb
+++ instagram/config/application.rb
@@ -16,7 +16,7 @@ module Instagram
     # -- all .rb files in that directory are automatically loaded.
     config.paperclip_defaults = {
       storage: :s3,
-      s3_region: ENV["s3_region"],
+      s3_region: ENV["us-west-1"],
       s3_credentials: {
         bucket: ENV["s3_bucket"],
         access_key_id: ENV["s3_access_key_id"],
