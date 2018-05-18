40d44e532426bba090f4f200a40a7f33a4fa87fd
Todd <toddleebergman@gmail.com>
2018-05-18 14:52:54 -0700

    edits to follows controller

--- instagram/app/controllers/api/follows_controller.rb
+++ instagram/app/controllers/api/follows_controller.rb
@@ -1,7 +1,23 @@
 class Api::FollowsController < ApplicationController
   def create
+    @follow = Follow.new
+    @follow.follower_id = current_user.id
+    @follow.followee_id = params[:id]
+    @user = User.find_by(id: params[:id])
+    if @follow.save
+      render :show
+    else
+      render json: @follow.errors.full_messages, status: 422
+    end
   end
 
   def destroy
+    @follow = Follow.find_by(follower_id: current_user.id, followee_id: params[:id])
+
+    if @follow.destroy
+      render :show
+    else
+      render json: @follow.errors.full_messages, status: 422
+    end
   end
 end
